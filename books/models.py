85fd447b1be055a2f3c28efae9590b67f308ee9a
valik_plunk <plunkrrr@gmail.com>
2012-05-09 13:01:30 +0300

    added books app

---
+++ books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    owner = models.ForeignKey(User)
+    #created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
