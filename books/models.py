6fe043d44b148c474c83219ef40744723ee52f99
valik_plunk <plunkrrr@gmail.com>
2012-05-09 13:05:47 +0300

    book model and settings.py fix

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Book(models.Model):
     title = models.CharField(max_length=256)
     owner = models.ForeignKey(User)
-    #created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.title
--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'users'
+    'users',
+    'books'
 )
 
 # A sample logging configuration. The only tangible logging
