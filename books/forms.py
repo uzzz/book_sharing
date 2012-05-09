ded9bcac9c0ed1bbd34d6511177b6b1baad8fc69
uzzz <uzzable@gmail.com>
2012-05-09 16:21:20 +0300

    changed lstrip() + rstrip() to strip()

--- books/forms.py
+++ books/forms.py
@@ -2,12 +2,13 @@ from django import forms
 
 class BooksImportForm(forms.Form):
     books = forms.CharField(widget=forms.Textarea)
+
     @staticmethod
     def parse_input_data(DataFromForm):
         rawBookTitles = DataFromForm.split('\n',1)
         normalisedBookTitles = []
         for rawTitle in rawBookTitles:
-            strippedTitle = rawTitle.rstrip()
-            strippedTitle = strippedTitle.lstrip()
+            strippedTitle = rawTitle.strip()
             normalisedBookTitles.append(strippedTitle)
+
         return normalisedBookTitles
