40e8abeb30052ef7db2f15aac19a876bbbd09617
uzzz <uzzable@gmail.com>
2012-05-09 16:17:57 +0300

    my books list view & delete

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, url
-from views import import_books
+from django.contrib.auth.decorators import login_required
+
+from views import *
 
 urlpatterns = patterns('',
-    url(r'import/$', import_books, name='import_books')
+    url(r'import/$', import_books, name='import_books'),
+    url(r'my/$', login_required(MyBooksListView.as_view()), name='my_books'),
+    url(r'(?P<pk>\d+)/delete/$', login_required(MyBookDeleteView.as_view()),
+        name='delete_my_book')
 )
--- books/views.py
+++ books/views.py
@@ -1,12 +1,14 @@
 from forms import BooksImportForm
+from models import Book
 
 from django.http import HttpRequest
 from django.http import HttpResponseRedirect
+from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from models import Book
-
+from django.views.generic import ListView
+from django.views.generic import DeleteView
 
 @login_required
 def import_books(request):
@@ -27,3 +29,21 @@ def import_books(request):
     return render_to_response('import_books.html', {
         'form': form,
     }, context_instance=RequestContext(request))
+
+
+class MyBooksListView(ListView):
+    context_object_name = 'books'
+    template_name = 'books_list.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(owner_id=self.request.user.id)
+
+class MyBookDeleteView(DeleteView):
+    model = Book
+    success_url = '/books/my'
+
+    def get_object(self, queryset=None):
+        book = super(MyBookDeleteView, self).get_object()
+        if not book.owner == self.request.user:
+            raise Http404
+        return book
