75894cbf377826410a3ea5a833aece9e8b4a9424
uzzz <uzzable@gmail.com>
2012-05-09 12:39:28 +0300

    renamed show.html to user_detail.html

--- users/tests/views.py
+++ users/tests/views.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from django.test.client import Client
 from factories import UserFactory
 
-class UserViewTest(TestCase):
+class UserViewTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = UserFactory.create()
 
-    def testUserDetail(self):
-        response = self.client.get('/users/' + str(self.user.pk) + '/')
+    def test_user_detail(self):
+        response = self.client.get('/users/%d/' % self.user.pk)
+
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.user.username)
--- users/views.py
+++ users/views.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 from django.template.loader import get_template
 
 class ShowDetailView(DetailView):
-    template_name = "show.html"
+    template_name = "user_detail.html"
     context_object_name = "user"
     model = User
