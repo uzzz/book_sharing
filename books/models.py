1692b8b837e4c3df2dd45cd697795ba3357881cb
valik_plunk <plunkrrr@gmail.com>
2012-05-12 17:22:40 +0300

    States witj django-fsm for book model

--- books/models.py
+++ books/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_fsm.db.fields import FSMField, transition
 
 class Book(models.Model):
     title = models.CharField(max_length=256)
     owner = models.ForeignKey(User)
     created = models.DateTimeField(auto_now_add=True)
+    state = FSMField(default='free')
 
     def __unicode__(self):
         return self.title
